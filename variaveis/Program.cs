9c37c67e37664db5fd059eea36f6653fe2b5e28f
Carolina Ocon <Carolina Ocon@SB-1490656>
2025-09-16 09:59:35 -0300

    Adicionar arquivos de projeto.

---
+++ variaveis/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// String para armazenar texto
+string nome = "Carol Ocon";
+
+// tipo de variavel para valores inteiros: 1, 2 e etc
+int idade = 16;
+
+//tipo de variavel para valores decimais: 1.5, 2.5 e etc
+float altura = 1.56f;
+double preço = 15.6;
+decimal saldo = 1000.50m;
+
+// tipo de variaveis para valores de logicos: true ou false 
+bool estudantes = true;
+
+// tipo de variaveis para valores unicos: 'A', 'B' e etc
+char genero = 'F';
+
+    //tipo de variavel para valores constantes: PI = 3.14
+    const double pi = 3.14;
+
+// Form 1
+Console.WriteLine(nome);
+
+// For 2
+Console.WriteLine($"0 {nome} tem {idade} anos ");
+
+// Form 3
+Console.WriteLine("0 " + nome + " tem " + idade + " anos. ");
+
+// semelhante ao <br> do HTML que ja utilizamos 
+Console.WriteLine($"\n o valor de pi é: {pi}");
+
+
+
+
+
+
+
